1beaaaa9d62c189f9ba08cbf33bc47b5fd81c1db
Tomás <tmontes@miuandes.cl>
2020-03-22 17:39:52 -0400

    person class and random

--- lab01tomasmontes/Program.cs
+++ lab01tomasmontes/Program.cs
@@ -13,3 +13,24 @@ namespace lab01tomasmontes
         }
     }
 }
+
+public class Person
+{
+    private string Name;
+    private string Lastname;
+
+    public Person(string Name, string Lastname)
+    {
+        this.Name = Name;
+        this.Lastname = Lastname;
+    }
+
+    public void Tirar()
+    {
+        Random Aleatorio = new Random();
+        int Numero;
+        Numero = Aleatorio.Next(0, 3);
+        Console.WriteLine("el numero es:" + Numero);
+    }
+}
+
